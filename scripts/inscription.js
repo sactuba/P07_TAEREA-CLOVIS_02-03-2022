9960c16a6e74d04b229b2b3bfce961e2ba24485d
sactuba <taloamn85@gmail.com>
2022-03-05 15:51:56 +0100

    mise a jour de la page de connexion

--- scripts/inscription.js
+++ scripts/inscription.js
@@ -1,4 +1,12 @@
 function submitForm() {
-    console.log(form);
+    const nom = document.querySelector('#input-nom').value;
+    const prenom = document.querySelector('#input-prenom').value;
+    const mail = document.querySelector('#input-mail').value;
+    const mdp = document.querySelector('#input-mdp').value;
+    const mdpConfirmation = document.querySelector('#input-mdpConfirmation').value;
+    console.log(nom);
+    console.log(prenom);
+    console.log(mail);
+    console.log(mdp);
+    console.log(mdpConfirmation);
 }
-submitForm();
