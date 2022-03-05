47ac75a4a90fd12321e4e94bff9957144c08d516
sactuba <taloamn85@gmail.com>
2022-03-05 14:52:59 +0100

    mise a jour de la page des recette

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,20 @@
   const main = document.querySelector('#main');
-//console.log(main);
+;
 
  async function recipesCard() {
     for (let i = 0; i < recipes.length; i++) {
         //console.log(recipes[i].ingredients);
-        for (let index = 0; index < ingredients.length; index++) {
-            console.log(recipes[i].ingredients[index]);
+        for (let index = 0; index < recipes[i].ingredients.length; index++) {
+            //console.log(recipes[i].ingredients[index]);
+            const ingredientList = `
+            <p class="card-text desc-ingredients">${recipes[i].ingredients[index].ingredient}</p>
+            `
+            console.log(ingredientList);
+            //document.querySelector('.card-ingredients').innerHTML += ingredientList;
         }
     }
 
-      for (let i = 0; i < recipes.length; i++) {
+      for (let i = 0; i < recipes.length; i++ ) {
         //console.log(recipes[i]);
          const card = `<div class="col">
           <div class="card h-100">
@@ -17,16 +22,16 @@
             <div class="card__desc">
             <div class="card-ingredients">
             <h5 class="card-title">${recipes[i].name}</h5>
-            <p class="card-text ">${recipes[i].ingredients}</p>
+            <p class="card-text desc-ingredients">${recipes[i].ingredients[1].ingredient}</p>
             </div>
             <div class="card-preparation">
-            <h5 class="card-timer">${recipes[i].time} min</h5>
-                <p class="card-text">${recipes[i].description}</p>
+            <h5 class="card-timer"><i class="fa-regular fa-clock"></i> ${recipes[i].time} min</h5>
+                <p class="card-text desc-preparation">${recipes[i].description}</p>
               </div>
             </div>
           </div>
-        </div>
-        `
+        </div>`
+        
         main.innerHTML += card;
         }
   }
---
+++ scripts/inscription.js
@@ -0,0 +1,4 @@
+function submitForm() {
+    console.log(form);
+}
+submitForm();
